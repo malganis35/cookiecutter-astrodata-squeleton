93ac60bea46fcc759fc7a53a5cc7197065c0f16c
Cao Tri DO <caotri.do88@gmail.com>
2026-04-02 14:12:37 +0200

    refactor: add type hints and imports to LimitUploadSizeMiddleware dispatch and init methods

--- data-science/{{ cookiecutter.repo_name }}/src/{{ cookiecutter.package_name }}/api/middlewares.py
+++ data-science/{{ cookiecutter.repo_name }}/src/{{ cookiecutter.package_name }}/api/middlewares.py
@@ -1,20 +1,25 @@
 import os
+from collections.abc import Awaitable, Callable
+
 from fastapi import Request, Response
+from loguru import logger
 from starlette.middleware.base import BaseHTTPMiddleware
 from starlette.responses import JSONResponse
-from loguru import logger
+from starlette.types import ASGIApp
 
 class LimitUploadSizeMiddleware(BaseHTTPMiddleware):
     """
     Middleware to limit the size of incoming requests.
     Rejects requests with a Content-Length greater than the specified limit.
     """
-    def __init__(self, app, max_upload_size: int = None):
+    def __init__(self, app: ASGIApp, max_upload_size: int | None = None) -> None:
         super().__init__(app)
         # Default to 50MB if not specified
         self.max_upload_size = max_upload_size or int(os.getenv("API_MAX_UPLOAD_SIZE", 52428800))
 
-    async def dispatch(self, request: Request, call_next):
+    async def dispatch(
+        self, request: Request, call_next: Callable[[Request], Awaitable[Response]]
+    ) -> Response:
         content_length = request.headers.get("content-length")
         
         if content_length:
